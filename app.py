800663e6828914d96064553f6071a95a1eb29d32
ArashAr <arash18ar@gmail.com>
2023-12-09 16:24:35 +0330

    Create Login Page For User

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
 # کتاب خانه های استفاده شده
 
-from flask import Flask 
+from flask import Flask , flash , redirect , url_for
+
 from flask_wtf.csrf import CSRFProtect
 
+from flask_login import LoginManager
+
 #فایل ها
+
 from blueprints.admin import app as admin
+
 from blueprints.user import app as user
+
 from blueprints.general import app as general
+
 from config import SQLALCHEMY_DATABASE_URI , SECRET_KEY
+
 from extentions import db
 
+from models.user import User
+
 #رجیستر ها
+
 app = Flask(__name__)
 
 app.register_blueprint(admin)
+
 app.register_blueprint(user)
+
 app.register_blueprint(general)
 
 # کد ها
@@ -26,6 +39,21 @@ app.config["SECRET_KEY"] = SECRET_KEY
 db.init_app(app)
 
 csrf = CSRFProtect(app)
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id == user_id).first()
+
+@login_manager.unauthorized_handler
+def unauthorized ():
+    flash (' وارد حساب کاربری خود شوید ')
+    return redirect(url_for('user.login'))
+
 
 with app.app_context():
     db.create_all()
--- blueprints/user.py
+++ blueprints/user.py
@@ -1,14 +1,66 @@
 # کتاب خانه ها 
 
-from flask import Blueprint
+from flask import Blueprint , request , render_template , redirect , flash , url_for
+
+from flask_login import current_user , login_user 
+
+from passlib.hash import sha256_crypt
 
 #فایل ها 
 
 from models.user import User
 
+from extentions import db
+
+#کد ها 
+
 
 app = Blueprint('user',__name__)
 
-@app.route('/user/login')
-def user():
-    return ' user '
+@app.route('/user/login' , methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        if current_user.is_authenticated:
+            return redirect('/user/dashboard')
+        return render_template('user/login.html')
+    else:
+        register = request.form.get('register', None)
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        password2 = request.form.get('password2', None)
+        if register != None :
+            user = User.query.filter(User.username == username ).first()
+            if user != None:
+                flash(' نام کاربری دیگری انتخاب کنید ')
+                return redirect('/user/login')
+            
+            user = User(username = username , password = sha256_crypt.encrypt(password))
+
+            if password == password2 :
+                db.session.add(user)
+                db.session.commit()
+                login_user(user)
+                return redirect ('/user/dashboard')
+            else:
+                flash('رمز عبور مطابقت ندارد')
+                return redirect('/user/login')
+            
+        
+        else:
+            user = User.query.filter(User.username == username ).first()
+            if user == None:
+                flash(' نام کاربری یا رمز اشتباه است ')
+                return redirect(url_for('user.login'))
+            if sha256_crypt.verify( password , user.password  ):
+                login_user(user)
+                return redirect('/user/dashboard')
+            else :
+                flash(' نام کاربری یا رمز اشتباه است ')
+                return redirect('/user/login')
+
+
+
+
+@app.route('/user/dashboard')
+def dash():
+    return "Dashboard"
