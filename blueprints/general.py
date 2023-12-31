318d3df5975ef820a6e79097fe578b2cd9c2dfe7
ArashAr <arash18ar@gmail.com>
2023-12-31 16:44:43 +0330

    Finish Main Page

--- blueprints/general.py
+++ blueprints/general.py
@@ -8,6 +8,8 @@ from models.product import *
 
 from models.course import *
 
+from models.exprience import *
+
 app = Blueprint('general',__name__)
 
 
@@ -15,10 +17,18 @@ app = Blueprint('general',__name__)
 def main():
     if request.method == "GET":
         courses = Course.query.filter(Course.active==1).order_by(func.random()).limit(6).all()
-    return render_template('main.html', courses = courses)
+        experiences = Experience.query.filter(Experience.active ==1).order_by(func.random()).limit(5).all()
+    return render_template('main.html', courses = courses , experiences = experiences)
 
 @app.route('/course/<int:id>/<name>',methods=["GET"])
 def course(id,name):
     course = Course.query.filter(Course.id == id).filter(Course.name == name).filter(Course.active==1).first_or_404()
-    another_course = Course.query.filter(Course.name.like(f'%{course.name[0:3]}%')).order_by(func.random()).limit(3).all()
+    another_course = Course.query.order_by(func.random()).limit(3).all()
     return render_template('course.html',course=course,another_course=another_course)
+
+
+@app.route('/experience/<int:id>/<name>',methods=["GET"])
+def experience(id,name):
+    experience = Experience.query.filter(Experience.id == id).filter(Experience.name == name).filter(Experience.active==1).first_or_404()
+    another_experience = Experience.query.order_by(func.random()).limit(3).all()
+    return render_template('experience.html',experience=experience,another_experience=another_experience)
